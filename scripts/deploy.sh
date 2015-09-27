ec1be6f4132bfcdf8c38018c69941f24a10c8b43
y_uuki <yuki.tsubo@gmail.com>
2015-09-27 11:29:58 +0900

    mysql と nginx も再起動するように

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 IPADDR=$1
-ssh isucon@$IPADDR "cd /home/isucon/deploy && git pull && ~/deploy/env.sh carton install && sudo systemctl restart isuxi.perl"
+ssh isucon@$IPADDR "cd /home/isucon/deploy && git pull && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx"
