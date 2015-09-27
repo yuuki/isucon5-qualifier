bae02b3fc261a36e91aa7f7d6fcb621034e3bf9d
y_uuki <yuki.tsubo@gmail.com>
2015-09-27 15:25:57 +0900

    Revert "prebenchで再起動やめる"
    This reverts commit 1ee1cc67129f0740d96897d37b11f14e453bd39d.

--- scripts/prebench.sh
+++ scripts/prebench.sh
@@ -4,6 +4,10 @@ set -ex
 sudo rm -f /var/lib/mysql/mysqld-slow.log
 sudo rm -f /var/log/nginx/isucon5.access_log.tsv /var/log/nginx/access.log
 sudo rm -fr /tmp/nytprof.out.* /tmp/nytprof_report
+sudo systemctl restart mysql 
+sudo service memcached restart
+sudo systemctl restart isuxi.perl
+sudo systemctl restart nginx
 
 # sudo supervisorctl stop isucon_perl # for nytprof
 # sudo systemctl restart isuxi.perl
