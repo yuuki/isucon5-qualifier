9358f6f4d0c1baf9744e70b81810efe2ab53d879
y_uuki <yuki.tsubo@gmail.com>
2015-09-27 12:48:25 +0900

    memcached の再起動もいれる

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 set -ex
 IPADDR=$1
 USERNAME=$USER
-ssh isucon@$IPADDR "/home/isucon/notify.sh $USERNAME 'deploying...' && cd /home/isucon/deploy && git pull && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx && sudo sysctl -p && /home/isucon/notify.sh $USERNAME 'deploy done'"
+ssh isucon@$IPADDR "/home/isucon/notify.sh $USERNAME 'deploying...' && cd /home/isucon/deploy && git pull && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo service memcached restart && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx && sudo sysctl -p && /home/isucon/notify.sh $USERNAME 'deploy done'"
