981c8899eda74936b656e6aebd4c2b8f8f90fb04
y_uuki <yuki.tsubo@gmail.com>
2015-09-27 15:57:44 +0900

    prebenchでログ退避するように

--- scripts/prebench.sh
+++ scripts/prebench.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -ex
 
-sudo rm -f /var/lib/mysql/mysqld-slow.log
-sudo rm -f /var/log/nginx/isucon5.access_log.tsv /var/log/nginx/access.log
+if [ -f /var/lib/mysql/mysqld-slow.log ]; then
+    sudo mv /var/lib/mysql/mysqld-slow.log /var/lib/mysql/mysqld-slow.log.$(date "+%Y%m%d_%H%M%S")
+fi
+if [ -f /var/log/nginx/isucon5.access_log.tsv ]; then
+    sudo mv /var/log/nginx/isucon5.access_log.tsv /var/log/nginx/isucon5.access_log.tsv.$(date "+%Y%m%d_%H%M%S")
+fi
 sudo rm -fr /tmp/nytprof.out.* /tmp/nytprof_report
 sudo systemctl restart mysql 
 sudo service memcached restart
 sudo systemctl restart isuxi.perl
 sudo systemctl restart nginx
 
-# sudo supervisorctl stop isucon_perl # for nytprof
-# sudo systemctl restart isuxi.perl
