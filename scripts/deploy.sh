9aa2c975d577136e8103213e656a10c2682a73ea
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-10-25 18:12:52 +0900

    restart redis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ set -ex
 IPADDR=$1
 USERNAME=$USER
 REVISION=${2:-`git log -n 1 --format='%h'`}
-ssh isucon@$IPADDR "/home/isucon/notify.sh $USERNAME 'deploying...' && cd /home/isucon/deploy && git remote update && git checkout $REVISION && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo service memcached restart && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx && sudo sysctl -p && /home/isucon/notify.sh $USERNAME 'deploy done'"
+ssh isucon@$IPADDR "/home/isucon/notify.sh $USERNAME 'deploying...' && cd /home/isucon/deploy && git remote update && git checkout $REVISION && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo service redis-server restart && sudo service memcached restart && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx && sudo sysctl -p && /home/isucon/notify.sh $USERNAME 'deploy done'"
