947ff5dc3b1d7acb265b4d332a21b40fafb6a583
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-10-25 16:40:42 +0900

    force checkout

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,5 @@
 set -ex
 IPADDR=$1
 USERNAME=$USER
-ssh isucon@$IPADDR "/home/isucon/notify.sh $USERNAME 'deploying...' && cd /home/isucon/deploy && git pull && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo service memcached restart && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx && sudo sysctl -p && /home/isucon/notify.sh $USERNAME 'deploy done'"
+REVISION=${2:-`git log -n 1 --format='%h'`}
+ssh isucon@$IPADDR "/home/isucon/notify.sh $USERNAME 'deploying...' && cd /home/isucon/deploy && git remote update && git checkout $REVISION && ~/deploy/env.sh carton install && sudo systemctl restart mysql && sudo service memcached restart && sudo systemctl restart isuxi.perl && sudo systemctl restart nginx && sudo sysctl -p && /home/isucon/notify.sh $USERNAME 'deploy done'"
